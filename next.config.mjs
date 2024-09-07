2f0ff8d0bee68b54689d9a1faff870984606fe11
Kelvin <muhammadfalahalfarisi@gmail.com>
2024-09-07 11:51:50 +0800

    delete comentar code inmy next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,6 @@
 const nextConfig = {
     images: {
         remotePatterns: [
-            // {
-            //     protocol: "https",
-            //     hostname: "vlhp7vnbo3iooptf.public.blob.vercel-storage.com",
-            // },
             {
                 protocol: "https",
                 hostname: "ovsyycftgdiyx8vh.public.blob.vercel-storage.com",
@@ -16,32 +12,3 @@ const nextConfig = {
 };
 
 export default nextConfig;
-
-// const nextConfig = {
-//     images:{
-//         remotePattern:[
-//             {
-//                 protocol: 'https',
-//                 hostname:'',
-//             }
-//         ]
-//     }
-// };
-
-// export default nextConfig;
-// await import("./src/env.js");
-
-// /** @type {import("next").NextConfig} */
-// const config = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "github.com",
-//       },
-//     ],
-//   },
-// };
-
-// export default config;
-
